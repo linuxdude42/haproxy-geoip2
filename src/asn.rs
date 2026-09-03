784dc5cdffa46caa17e397f9fb5e1c0228808413
David Hampton <mythtv@love2code.net>
2026-09-03 17:20:17 -0400

    Update to support maxminddb version 0.30.
    Also add support for extracting the network mask for
    the matched network.

--- src/asn.rs
+++ src/asn.rs
@@ -1,6 +1,7 @@
 use std::net::IpAddr;
 
 use maxminddb::geoip2::Asn;
+use maxminddb::LookupResult;
 use mlua::prelude::{IntoLua, Lua, LuaValue};
 
 use crate::db::Database;
@@ -14,12 +15,21 @@ pub(crate) fn lookup<'a>(lua: &'a Lua, ip: IpAddr, props: &[String]) -> Option<L
 
     let db = DB.load();
     let reader = db.as_ref()?;
-    let asn = reader.lookup::<Asn>(ip).ok().flatten()?;
-    lookup_asn(&asn, props).and_then(|v| v.into_lua(lua).ok())
+    let lookup = reader.lookup(ip).ok()?;
+    let asn = lookup.decode::<Asn>().ok().flatten()?;
+    lookup_asn(&lookup, &asn, props).and_then(|v| v.into_lua(lua).ok())
 }
 
-fn lookup_asn<'a>(asn: &'a Asn, props: &[String]) -> Option<GeoValue<'a>> {
+fn lookup_asn<'a>(lookup: &LookupResult<'a, Vec<u8>>, asn: &'a Asn, props: &[String]) -> Option<GeoValue<'a>> {
     match props.get(0)?.as_str() {
+	"mask" | "network_mask" | "prefix_len" => {
+            match lookup.network() {
+                Ok(network) => {
+                    return Some(GeoValue::UInt(network.prefix() as u32));
+                    }
+                Err(_) => return None,
+            };
+        }
         "autonomous_system_number" | "asn" => {
             if let Some(number) = asn.autonomous_system_number {
                 return Some(GeoValue::UInt(number));
--- src/city.rs
+++ src/city.rs
@@ -2,6 +2,7 @@ use std::net::IpAddr;
 
 use maxminddb::geoip2::city::Country;
 use maxminddb::geoip2::City;
+use maxminddb::LookupResult;
 use mlua::prelude::{IntoLua, Lua, LuaValue};
 
 use crate::db::Database;
@@ -15,60 +16,58 @@ pub(crate) fn lookup<'a>(lua: &Lua, ip: IpAddr, props: &[String]) -> Option<LuaV
 
     let db = DB.load();
     let reader = db.as_ref()?;
-    let city = reader.lookup::<City>(ip).ok().flatten()?;
-    lookup_city(&city, props).and_then(|v| v.into_lua(lua).ok())
+    let lookup = reader.lookup(ip).ok()?;
+    let city = lookup.decode::<City>().ok().flatten()?;
+    lookup_city(&lookup, &city, props).and_then(|v| v.into_lua(lua).ok())
 }
 
-fn lookup_city<'a>(city: &'a City, props: &[String]) -> Option<GeoValue<'a>> {
+fn lookup_city<'a>(lookup: &LookupResult<'a, Vec<u8>>, city: &'a City, props: &[String]) -> Option<GeoValue<'a>> {
     match props.get(0)?.as_str() {
+	"mask" | "network_mask" | "prefix_len" => {
+            match lookup.network() {
+                Ok(network) => {
+                    return Some(GeoValue::UInt(network.prefix() as u32));
+                    }
+                Err(_) => return None,
+            };
+        }
         "city" => match props.get(1).map(|s| s.as_str()) {
             Some("names") => {
-                if let Some(city) = city.city.as_ref().and_then(|c| c.names.as_ref()) {
-                    if let Some(&value) = props.get(2).and_then(|lang| city.get(lang.as_str())) {
-                        return Some(GeoValue::Str(value));
-                    }
-                }
+                match props.get(2)?.as_str() {
+                    "de"    => city.city.names.german.map(GeoValue::Str),
+                    "en"    => city.city.names.english.map(GeoValue::Str),
+                    "es"    => city.city.names.spanish.map(GeoValue::Str),
+                    "fr"    => city.city.names.french.map(GeoValue::Str),
+                    "ja"    => city.city.names.japanese.map(GeoValue::Str),
+                    "pt-BR" => city.city.names.brazilian_portuguese.map(GeoValue::Str),
+                    "ru"    => city.city.names.russian.map(GeoValue::Str),
+                    "zh-CN" => city.city.names.simplified_chinese.map(GeoValue::Str),
+                    _ =>       city.city.names.english.map(GeoValue::Str),
+                };
             }
             Some(_) => {}
             None => {
-                if let Some(&value) = (city.city.as_ref())
-                    .and_then(|c| c.names.as_ref())
-                    .and_then(|names| names.get("en"))
-                {
-                    return Some(GeoValue::Str(value));
-                }
+                return city.city.names.english.map(GeoValue::Str)
             }
         },
         "country" => {
-            return city
-                .country
-                .as_ref()
-                .and_then(|c| lookup_country(c, &props[1..]));
+            return lookup_country(&city.country, &props[1..]);
         }
         "registered_country" => {
-            return city
-                .registered_country
-                .as_ref()
-                .and_then(|c| lookup_country(c, &props[1..]));
+            return lookup_country(&city.registered_country, &props[1..]);
         }
         "location" => match props.get(1).map(|s| s.as_str()) {
             Some("latitude") => {
-                if let Some(lat) = city.location.as_ref().and_then(|l| l.latitude) {
-                    return Some(GeoValue::Float(lat));
-                }
+                return city.location.latitude.map(GeoValue::Float)
             }
             Some("longitude") => {
-                if let Some(lon) = city.location.as_ref().and_then(|l| l.longitude) {
-                    return Some(GeoValue::Float(lon));
-                }
+                return city.location.longitude.map(GeoValue::Float);
             }
             Some("timezone") => {
-                if let Some(tz) = city.location.as_ref().and_then(|l| l.time_zone) {
-                    return Some(GeoValue::Str(tz));
-                }
+                return city.location.time_zone.map(GeoValue::Str);
             }
             Some("metro_code") => {
-                if let Some(code) = city.location.as_ref().and_then(|l| l.metro_code) {
+                if let Some(code) = city.location.metro_code {
                     return Some(GeoValue::UInt(code as u32));
                 }
             }
@@ -76,41 +75,47 @@ fn lookup_city<'a>(city: &'a City, props: &[String]) -> Option<GeoValue<'a>> {
         },
         "postal" => match props.get(1).map(|s| s.as_str()) {
             Some("code") | None => {
-                if let Some(code) = city.postal.as_ref().and_then(|p| p.code) {
-                    return Some(GeoValue::Str(code));
-                }
+                return city.postal.code.map(GeoValue::Str);
             }
             _ => {}
         },
         "continent" => match props.get(1).map(|s| s.as_str()) {
             Some("code") | None => {
-                if let Some(code) = city.continent.as_ref().and_then(|c| c.code) {
-                    return Some(GeoValue::Str(code));
-                }
+                return city.continent.code.map(GeoValue::Str);
             }
             Some("names") => {
-                if let Some(continent) = city.continent.as_ref().and_then(|c| c.names.as_ref()) {
-                    if let Some(&value) = props.get(2).and_then(|lang| continent.get(lang.as_str()))
-                    {
-                        return Some(GeoValue::Str(value));
-                    }
-                }
+                match props.get(2)?.as_str() {
+                    "de"    => city.continent.names.german.map(GeoValue::Str),
+                    "en"    => city.continent.names.english.map(GeoValue::Str),
+                    "es"    => city.continent.names.spanish.map(GeoValue::Str),
+                    "fr"    => city.continent.names.french.map(GeoValue::Str),
+                    "ja"    => city.continent.names.japanese.map(GeoValue::Str),
+                    "pt-BR" => city.continent.names.brazilian_portuguese.map(GeoValue::Str),
+                    "ru"    => city.continent.names.russian.map(GeoValue::Str),
+                    "zh-CN" => city.continent.names.simplified_chinese.map(GeoValue::Str),
+                    _ =>       city.continent.names.english.map(GeoValue::Str),
+                };
             }
             _ => {}
         },
         "subdivisions" | "subdivision" => {
-            if let Some(subdivisions) = city.subdivisions.as_ref() {
+            let subdivisions = &city.subdivisions;
+            {
                 if let Some(index) = props.get(1).and_then(|s| s.parse::<usize>().ok()) {
                     if let Some(subdivision) = subdivisions.get(index) {
                         match props.get(2).map(|s| s.as_str()) {
                             Some("names") => {
-                                if let Some(names) = subdivision.names.as_ref() {
-                                    if let Some(&value) =
-                                        props.get(3).and_then(|lang| names.get(lang.as_str()))
-                                    {
-                                        return Some(GeoValue::Str(value));
-                                    }
-                                }
+                                match props.get(3)?.as_str() {
+                                    "de"    => subdivision.names.german.map(GeoValue::Str),
+                                    "en"    => subdivision.names.english.map(GeoValue::Str),
+                                    "es"    => subdivision.names.spanish.map(GeoValue::Str),
+                                    "fr"    => subdivision.names.french.map(GeoValue::Str),
+                                    "ja"    => subdivision.names.japanese.map(GeoValue::Str),
+                                    "pt-BR" => subdivision.names.brazilian_portuguese.map(GeoValue::Str),
+                                    "ru"    => subdivision.names.russian.map(GeoValue::Str),
+                                    "zh-CN" => subdivision.names.simplified_chinese.map(GeoValue::Str),
+                                    _ =>       subdivision.names.english.map(GeoValue::Str),
+                                };
                             }
                             Some("iso_code") | None => {
                                 if let Some(code) = subdivision.iso_code {
@@ -124,22 +129,11 @@ fn lookup_city<'a>(city: &'a City, props: &[String]) -> Option<GeoValue<'a>> {
             }
         }
         "traits" => {
-            if let Some(traits) = city.traits.as_ref() {
+            let traits = &city.traits;
+            {
                 match props.get(1).map(|s| s.as_str()) {
-                    Some("is_anonymous_proxy") => {
-                        if let Some(is_anonymous_proxy) = traits.is_anonymous_proxy {
-                            return Some(GeoValue::Bool(is_anonymous_proxy));
-                        }
-                    }
                     Some("is_anycast") => {
-                        if let Some(is_anycast) = traits.is_anycast {
-                            return Some(GeoValue::Bool(is_anycast));
-                        }
-                    }
-                    Some("is_satellite_provider") => {
-                        if let Some(is_satellite_provider) = traits.is_satellite_provider {
-                            return Some(GeoValue::Bool(is_satellite_provider));
-                        }
+                        return traits.is_anycast.map(GeoValue::Bool);
                     }
                     _ => {}
                 }
@@ -153,11 +147,17 @@ fn lookup_city<'a>(city: &'a City, props: &[String]) -> Option<GeoValue<'a>> {
 fn lookup_country<'a>(country: &'a Country, props: &[String]) -> Option<GeoValue<'a>> {
     match props.get(0).map(|s| s.as_str()) {
         Some("names") => {
-            if let Some(country) = country.names.as_ref() {
-                if let Some(&value) = props.get(2).and_then(|lang| country.get(lang.as_str())) {
-                    return Some(GeoValue::Str(value));
-                }
-            }
+            match props.get(2)?.as_str() {
+                "de"    => country.names.german.map(GeoValue::Str),
+                "en"    => country.names.english.map(GeoValue::Str),
+                "es"    => country.names.spanish.map(GeoValue::Str),
+                "fr"    => country.names.french.map(GeoValue::Str),
+                "ja"    => country.names.japanese.map(GeoValue::Str),
+                "pt-BR" => country.names.brazilian_portuguese.map(GeoValue::Str),
+                "ru"    => country.names.russian.map(GeoValue::Str),
+                "zh-CN" => country.names.simplified_chinese.map(GeoValue::Str),
+                _ =>       country.names.english.map(GeoValue::Str),
+            };
         }
         Some("iso_code") | None => {
             if let Some(code) = country.iso_code {
